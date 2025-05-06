7b272ddcf7320c6a9bdeff83fdd5743fb069446f
KristapsTucs <kristapstucs1@gmail.com>
2025-05-06 22:15:30 +0300

    fgas

---
+++ benzins2/src/views/GraphView.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="app">
+      <h1>Hello Vue 3!</h1>
+      <p>Counter: {{ count }}</p>
+      <button @click="increment">Increment</button>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue'
+  
+  const count = ref(0)
+  const increment = () => {
+    count.value++
+  }
+  </script>
+  
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    text-align: center;
+    margin-top: 40px;
+  }
+  </style>
+  
