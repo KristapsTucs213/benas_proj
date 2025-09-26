79eed9a553335b8823a1104d597bb65f6be65702
KristapsTucs <kristapstucs1@gmail.com>
2025-09-26 17:56:10 +0300

    added login and register with encryption

--- benzins2/src/App.vue
+++ benzins2/src/App.vue
@@ -2,11 +2,47 @@
   <nav>
     <router-link to="/">Benzins</router-link> |
     <router-link to="/cenasvesture">Graphs</router-link> |
-    <router-link to="/register">Register</router-link> |
-    <router-link to="/login">Login</router-link> |
+
+    <!-- Show Register/Login only if user is NOT logged in -->
+    <template v-if="!isLoggedIn">
+      <router-link to="/register">Register</router-link> |
+      <router-link to="/login">Login</router-link> |
+    </template>
+
+    <!-- Show Logout button if user IS logged in -->
+    <template v-else>
+      <span>Welcome!</span> |
+      <a href="#" @click.prevent="logout">Logout</a> |
+    </template>
   </nav>
-   <router-view/>
+
+  <router-view />
 </template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      isLoggedIn: false,
+    };
+  },
+  mounted() {
+    // Check localStorage on page load
+    this.isLoggedIn = !!localStorage.getItem("userId");
+  },
+  methods: {
+    logout() {
+      // Remove user ID from localStorage
+      localStorage.removeItem("userId");
+      this.isLoggedIn = false;
+
+      // Refresh the page
+      window.location.reload();
+    },
+  },
+};
+</script>
 
 <style>
 #app {
@@ -16,7 +52,6 @@
   text-align: center;
   color: #2c3e50;
 }
-
 
 /* Navbar overlay */
 nav {
@@ -38,5 +73,4 @@ nav a {
 nav a.router-link-exact-active {
   color: #42b983;
 }
-
 </style>
--- benzins2/src/views/RegisterView.vue
+++ benzins2/src/views/RegisterView.vue
@@ -1,18 +1,83 @@
-
 <template>
   <div class="register-container">
     <h1>Register</h1>
-    <form>
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
+    <form @submit.prevent="register">
+      <input
+        type="text"
+        v-model="first_name"
+        placeholder="First Name"
+        required
+      />
+      <input
+        type="text"
+        v-model="last_name"
+        placeholder="Last Name"
+        required
+      />
+      <input
+        type="email"
+        v-model="email"
+        placeholder="Email"
+        required
+        :class="{ 'input-error': emailError }"
+      />
+      <span v-if="emailError" class="error-message">{{ emailError }}</span>
+
+      <input
+        type="password"
+        v-model="password"
+        placeholder="Password"
+        required
+      />
       <button type="submit">Register</button>
     </form>
+
+    <p v-if="successMessage" class="success-message">{{ successMessage }}</p>
   </div>
 </template>
 
 <script>
-import '@/css/RegisterView.css'
+import axios from "axios";
+import "@/css/RegisterView.css";
+
 export default {
   name: "RegisterView",
+  data() {
+    return {
+      first_name: "",
+      last_name: "",
+      email: "",
+      password: "",
+      emailError: "",
+      successMessage: "",
+    };
+  },
+  methods: {
+    async register() {
+      this.emailError = "";
+      this.successMessage = "";
+
+      try {
+        const response = await axios.post("http://localhost:5001/register", {
+          first_name: this.first_name,
+          last_name: this.last_name,
+          email: this.email,
+          password: this.password,
+        });
+
+        this.successMessage = response.data.message;
+        this.first_name = "";
+        this.last_name = "";
+        this.email = "";
+        this.password = "";
+      } catch (error) {
+        if (error.response && error.response.status === 409) {
+          this.emailError = error.response.data.error;
+        } else {
+          this.emailError = "Registration failed. Email already in use.";
+        }
+      }
+    },
+  },
 };
 </script>
